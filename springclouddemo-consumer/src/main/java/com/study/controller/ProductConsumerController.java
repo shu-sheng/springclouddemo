cecd33895090e555e83fa482bbec055572a543ac
shusheng <174185499@qq.com>
2019-08-08 15:55:04 +0800

    消费者系统更新请求方式，实现登录

--- springclouddemo-consumer/src/main/java/com/study/controller/ProductConsumerController.java
+++ springclouddemo-consumer/src/main/java/com/study/controller/ProductConsumerController.java
@@ -1,6 +1,9 @@
 package com.study.controller;
 
 import com.study.pojo.Product;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -13,21 +16,26 @@ import java.util.List;
 public class ProductConsumerController {
 
     public static final String PRODUCT_GET_URL = "http://localhost:8081/product/get/";
-    public static final String PRODUCT_LIST_URL="http://localhost:8081/product/list";
+    public static final String PRODUCT_LIST_URL = "http://localhost:8081/product/list";
 
     @Resource
     private RestTemplate restTemplate;
 
+    @Resource
+    private HttpHeaders httpHeaders;
+
     @RequestMapping("/product/get")
     public Object getProduct(long id) {
-        Product product = restTemplate.getForObject(PRODUCT_GET_URL + id, Product.class);
-        return  product;
+        Product product = restTemplate.exchange(PRODUCT_GET_URL + id, HttpMethod.GET,
+                new HttpEntity<Object>(httpHeaders), Product.class).getBody();
+        return product;
     }
 
     @RequestMapping("/product/list")
-    public  Object listProduct() {
-        List<Product> list = restTemplate.getForObject(PRODUCT_LIST_URL,List.class);
-        return  list;
+    public Object listProduct() {
+        List<Product> list = restTemplate.exchange(PRODUCT_LIST_URL, HttpMethod.GET,
+                new HttpEntity<Object>(httpHeaders), List.class).getBody();
+        return list;
     }
 
 }
