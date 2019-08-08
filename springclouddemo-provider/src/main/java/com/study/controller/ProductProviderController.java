521eccd29d7c8660169893c88c5fd0a176ee8364
shusheng <174185499@qq.com>
2019-08-08 19:50:50 +0800

    对于注册到 Eureka 上的服务，可以通过发现服务来获取一些服务信息

--- springclouddemo-provider/src/main/java/com/study/controller/ProductProviderController.java
+++ springclouddemo-provider/src/main/java/com/study/controller/ProductProviderController.java
@@ -2,6 +2,7 @@ package com.study.controller;
 
 import com.study.pojo.Product;
 import com.study.service.ProductService;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,9 @@ public class ProductProviderController {
     @Resource
     private ProductService productService;
 
+    @Resource
+    private DiscoveryClient client ; // 进行Eureka的发现服务
+
     @RequestMapping("/get/{id}")
     public Object get(Integer id) {
         return this.productService.get(id);
@@ -26,6 +30,11 @@ public class ProductProviderController {
         List<Product> productList = new ArrayList<>();
         productList = this.productService.list();
         return productList;
+    }
+
+    @RequestMapping("/discover")
+    public Object discover() { // 直接返回发现服务信息
+        return this.client ;
     }
 
 }
