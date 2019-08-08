eb5340acf43f1deb7553804b3b596c05f12ba9bd
shusheng <174185499@qq.com>
2019-08-08 21:03:27 +0800

    用3个Eureka相互注册,实现Eureka的高可用

--- springclouddemo-eurekaserver1/src/main/java/com/study/EurekaApp1.java
+++ springclouddemo-eurekaserver1/src/main/java/com/study/EurekaApp1.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApp {
+public class EurekaApp1 {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApp.class,args);
+        SpringApplication.run(EurekaApp1.class,args);
     }
 
 }
