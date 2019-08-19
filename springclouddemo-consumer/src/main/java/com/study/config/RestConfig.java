113e0450295dda5488d2a19e999d1728f6e98dd9
shusheng <174185499@qq.com>
2019-08-19 19:39:08 +0800

    单独设置某个Ribbon的路由

--- springclouddemo-consumer/src/main/java/com/study/config/RestConfig.java
+++ springclouddemo-consumer/src/main/java/com/study/config/RestConfig.java
@@ -30,9 +30,9 @@ public class RestConfig {
         return headers;
     }
 
-    @Bean
-    public IRule ribbonRule() { // 其中IRule就是所有规则的标准
-        return new com.netflix.loadbalancer.RandomRule(); // 随机的访问策略
-    }
+//    @Bean
+//    public IRule ribbonRule() { // 其中IRule就是所有规则的标准
+//        return new com.netflix.loadbalancer.RandomRule(); // 随机的访问策略
+//    }
 
 }
